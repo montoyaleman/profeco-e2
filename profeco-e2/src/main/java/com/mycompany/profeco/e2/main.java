df884a532c628ca9aa8c2d362d6a66ae8d36aa4a
montoyaleman <83248002+montoyaleman@users.noreply.github.com>
2025-05-16 09:55:42 -0700

    Pantallas Iniciales

---
+++ profeco-e2/src/main/java/com/mycompany/profeco/e2/main.java
@@ -0,0 +1,11 @@
+package com.mycompany.profeco.e2;
+
+import Pantallas.Login;
+
+public class main {
+
+    public static void main(String[] args) {
+        Login a = new Login();
+        a.setVisible(true);
+    }
+}
