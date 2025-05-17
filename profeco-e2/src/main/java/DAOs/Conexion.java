302e7b8fbf94d964e3657a907df056010aa75293
martinyocupicio228307 <martin.yocupicio228307@potros.itson.edu.mx>
2025-05-17 10:56:32 -0700

    Correcciones menores

--- profeco-e2/src/main/java/DAOs/Conexion.java
+++ profeco-e2/src/main/java/DAOs/Conexion.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
  * @author Usuario
  */
 public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/profeco-e2";
+    private static final String URL = "jdbc:mysql://localhost:3306/profeco";
     private static final String USER = "root";
     private static final String PASSWORD = ""; // O la contraseña que hayas definido
 
