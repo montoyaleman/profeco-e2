84ba4ef275fba79b10bd94d85a9b342192da3951
martinyocupicio228307 <martin.yocupicio228307@potros.itson.edu.mx>
2025-05-17 10:57:03 -0700

    Contraseña a base de datos agregada

--- profeco-e2/src/main/java/DAOs/Conexion.java
+++ profeco-e2/src/main/java/DAOs/Conexion.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 public class Conexion {
     private static final String URL = "jdbc:mysql://localhost:3306/profeco";
     private static final String USER = "root";
-    private static final String PASSWORD = ""; // O la contraseña que hayas definido
+    private static final String PASSWORD = "root"; // O la contraseña que hayas definido
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
